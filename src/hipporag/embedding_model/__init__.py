654072882989a03325d2bf211e96bcd9a2be7280
SongSxxx <songge1110@163.com>
2025-04-15 18:25:53 +0800

    add embedding models 0.0

--- src/hipporag/embedding_model/__init__.py
+++ src/hipporag/embedding_model/__init__.py
@@ -2,6 +2,10 @@ from .Contriever import ContrieverModel
 from .base import EmbeddingConfig, BaseEmbeddingModel
 from .GritLM import GritLMEmbeddingModel
 from .NVEmbedV2 import NVEmbedV2EmbeddingModel
+from .OpenAI import OpenAIEmbeddingModel
+from .JinaEmbeddingsV2SmallEn import JinaEmbeddingsV2SmallEnEmbeddingModel
+from .VoyageMultilingual2 import VoyageMultilingual2EmbeddingModel
+from .BgeEnIcl import BgeEnIclEmbeddingModel
 
 from ..utils.logging_utils import get_logger
 
@@ -15,4 +19,12 @@ def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2")
         return NVEmbedV2EmbeddingModel
     elif "contriever" in embedding_model_name:
         return ContrieverModel
+    elif "voyage-multilingual-2" in embedding_model_name:
+        return VoyageMultilingual2EmbeddingModel
+    elif "jinaai/jina-embeddings-v2-small-en" in embedding_model_name:
+        return JinaEmbeddingsV2SmallEnEmbeddingModel
+    elif "BAAI/bge-en-icl" in embedding_model_name:
+        return BgeEnIclEmbeddingModel
+    elif "text-embedding" in embedding_model_name:
+        return OpenAIEmbeddingModel
     assert False, f"Unknown embedding model name: {embedding_model_name}"
