26a85d51079b9b2285553228025018c7a2095823
SongSxxx <126457381+SongSxxx@users.noreply.github.com>
2025-04-16 17:55:19 +0800

    Update __init__.py

--- src/hipporag/embedding_model/__init__.py
+++ src/hipporag/embedding_model/__init__.py
@@ -3,6 +3,7 @@ from .base import EmbeddingConfig, BaseEmbeddingModel
 from .GritLM import GritLMEmbeddingModel
 from .NVEmbedV2 import NVEmbedV2EmbeddingModel
 from .OpenAI import OpenAIEmbeddingModel
+from .BgeEnIcl import BgeEnIclEmbeddingModel
 
 from ..utils.logging_utils import get_logger
 
@@ -15,6 +16,8 @@ def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2")
         return NVEmbedV2EmbeddingModel
     elif "contriever" in embedding_model_name:
         return ContrieverModel
+    elif "BAAI/bge-en-icl" in embedding_model_name:
+        return BgeEnIclEmbeddingModel
     elif "text-embedding" in embedding_model_name:
         return OpenAIEmbeddingModel
     assert False, f"Unknown embedding model name: {embedding_model_name}"
