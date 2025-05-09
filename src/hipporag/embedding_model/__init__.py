6ec5ed4e83663b09033f4a7472a1f0c996607f96
SongSxxx <songge1110@163.com>
2025-05-09 10:56:33 +0800

    merge embedding models

--- src/hipporag/embedding_model/__init__.py
+++ src/hipporag/embedding_model/__init__.py
@@ -4,6 +4,8 @@ from .GritLM import GritLMEmbeddingModel
 from .NVEmbedV2 import NVEmbedV2EmbeddingModel
 from .OpenAI import OpenAIEmbeddingModel
 from .BgeSmallEnV15 import BgeSmallEnV15Model
+from .SfrEmbeddingMistral import SfrEmbeddingMistralModel
+from .E5Mistral7B import E5Mistral7BInstructEmbeddingModel
 from ..utils.logging_utils import get_logger
 
 logger = get_logger(__name__)
@@ -17,6 +19,10 @@ def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2")
         return ContrieverModel
     elif "BAAI/bge-small-en-v1.5" in embedding_model_name:
         return BgeSmallEnV15Model
+    elif "Salesforce/SFR-Embedding-Mistral" in embedding_model_name:
+        return SfrEmbeddingMistralModel
+    elif "intfloat/e5-mistral-7b-instruct" in embedding_model_name:
+        return E5Mistral7BInstructEmbeddingModel
     elif "text-embedding" in embedding_model_name:
         return OpenAIEmbeddingModel
     assert False, f"Unknown embedding model name: {embedding_model_name}"
